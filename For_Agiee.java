5e6b8615639aaf64439967a3e9f81774ac88f01d
AKgalv1x <akgalvis@gmail.com>
2025-04-02 08:19:49 -0500

    CondicionalForAngiee

---
+++ For_Agiee.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package for_agiee;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class For_Agiee {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String animes[] = {"hxh", "bsd", "jshk", "Bnah"};
+        
+        for (int i=1; i < animes.length;i++){
+            System.out.println(animes[i]);
+        }     
+        }
+    
+}
